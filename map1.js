01181f09c7bf8bdd00955f43fe0263b3ae919dfd
Jacob-D-Hill <jab144@outlook.com>
2026-06-02 19:45:26 +1000

    updated to get MVP

--- map1.js
+++ map1.js
@@ -1,11 +1,40 @@
 var vg_1 = "js/map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#statmap", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
 var vg_2 = "js/population.vg.json";
 vegaEmbed("#pop", vg_2).then(function(result) {
 }).catch(console.error);
+
 var vg_3 = "js/zones.vg.json";
-vegaEmbed("#map2", vg_3).then(function(result) {
+vegaEmbed("#zonemap", vg_3).then(function(result) {
 }).catch(console.error);
 
+var vg_4 = "js/stations_per_country.vg.json";
+vegaEmbed("#station", vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "js/claim_area.vg.json";
+vegaEmbed("#claimarea", vg_5).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "js/stationTypes.vg.json";
+vegaEmbed("#statype", vg_6).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "js/features.vg.json";
+vegaEmbed("#feature", vg_7).then(function(result) {
+}).catch(console.error);
+
+var vg_8 = "js/protected_area.vg.json";
+vegaEmbed("#protm", vg_8).then(function(result) {
+}).catch(console.error);
+
+var vg_9 = "js/protected_area_map.vg.json";
+vegaEmbed("#prot", vg_9).then(function(result) {
+}).catch(console.error);
+
+var vg_0 = "js/boat.vg.json";
+vegaEmbed("#boat", vg_0).then(function(result) {
+}).catch(console.error);
